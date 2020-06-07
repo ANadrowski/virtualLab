4ae6d84233f6baa4f2151a7e13b42fe94d35da33
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2020-06-07 14:51:34 -0300

    Adds CalcForce

---
+++ src/main/java/br/com/anadrowski/virtuallab/formulas/mechanics/CalcForce.java
@@ -0,0 +1,68 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2020, Aislan Nadrowski
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package br.com.anadrowski.virtuallab.formulas.mechanics;
+
+import br.com.anadrowski.virtuallab.formulas.Formula;
+
+/**
+ * Calculates the Force (second Newton's law).
+ *
+ * @author Aislan Nadrowski (aislan.nadrowski@gmail.com)
+ * @since 0.0.1
+ */
+public class CalcForce implements Formula {
+
+    private final double mass;
+    private final double acceleration;
+
+    /**
+     * Constructor with parameters.
+     *
+     * @param mass Mass in kg.
+     * @param acceleration Acceleration in m/s2
+     */
+    public CalcForce(final double mass, final double acceleration) {
+        this.mass = mass;
+        this.acceleration = acceleration;
+    }
+
+    /**
+     * Calculates the force.
+     *
+     * @return Force.
+     */
+    @Override
+    public double calc() {
+        return this.mass * this.acceleration;
+    }
+
+}
