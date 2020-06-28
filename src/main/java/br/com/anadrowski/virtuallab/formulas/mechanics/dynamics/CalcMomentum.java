5fd1b4e0de7d648fb5268224dd8b1707fe4deb4e
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2020-06-27 21:53:06 -0300

    Adds CalcMomentum class

---
+++ src/main/java/br/com/anadrowski/virtuallab/formulas/mechanics/dynamics/CalcMomentum.java
@@ -0,0 +1,69 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2020, Aislan Nadrowski
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package br.com.anadrowski.virtuallab.formulas.mechanics.dynamics;
+
+import br.com.anadrowski.virtuallab.formulas.Formula;
+
+/**
+ * Calculates the momentum.
+ *
+ * @author Aislan Nadrowski
+ * (aislan.nadrowski@gmail.com)
+ * @since 0.0.1
+ */
+public class CalcMomentum implements Formula {
+
+  private final double mass;
+  private final double velocity;
+
+  /**
+   * Constructor with parameters.
+   *
+   * @param mass in kg
+   * @param velocity in m/s
+   */
+  public CalcMomentum(final double mass, final double velocity) {
+    this.mass = mass;
+    this.velocity = velocity;
+  }
+
+  /**
+   * Calculates the momentum.
+   *
+   * @return Momentum in kilogram meters per second (kg.m/s).
+   */
+  @Override
+  public double calc() {
+    return this.mass * this.velocity;
+  }
+
+}
